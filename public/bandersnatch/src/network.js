bf7a297490288b56a4f7dbd73bef311fa608b3e4
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2020-10-30 18:55:57 -0300

    third class

--- public/bandersnatch/src/network.js
+++ public/bandersnatch/src/network.js
@@ -7,8 +7,36 @@ class Network {
     return url.replace(fileResolutionTag, fileResolution).replace(hostTag, this.host)
   }
 
-  fetchFile(url) {
+  async fetchFile(url) {
     const response = await fetch(url)
     return response.arrayBuffer()
+  }
+
+  async getProperResolution(url) {
+    const startMs = Date.now()
+    const response = await fetch(url)
+    await response.arrayBuffer()
+    const endMs = Date.now()
+    const durationInMs = (endMs - startMs)
+
+    // ao invés de calcular o throughput vamos calcular pelo tempo
+    const resolutions = [
+      // pior cenário possível, 20s
+      { start: 3001, end: 20000, resolution: 144 },
+      // até 3s
+      { start: 901, end: 3000, resolution: 360 },
+      // menos de 1s
+      { start: 0, end: 900, resolution: 720 },
+    ]
+
+    const item = resolutions.find(item => {
+      return item.start <= durationInMs && item.end >= durationInMs
+    })
+
+    const LOWEST_RESOLUTION = 144
+    // se for mais de 30s
+    if (!item) return LOWEST_RESOLUTION
+    
+    return item.resolution
   }
 }
